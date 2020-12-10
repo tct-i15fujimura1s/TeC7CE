043f13959829dcbc2211f20eca9633cc1113316b
tct-i15fujimura1s <fujimura1s.i15.tct@gmail.com>
2020-12-10 14:36:54 +0900

    遷移

--- Vhdl/Sequencer.vhd
+++ Vhdl/Sequencer.vhd
@@ -52,46 +52,46 @@ architecture Behavioral of Sequencer is
 begin
 -- State machine
   with Stat select
-    DecSt <= "00000000000001" when "0000",
-             "00000000000010" when "0001",
-             "00000000000100" when "0010",
-             "00000000001000" when "0011",
-             "00000000010000" when "0100",
-             "00000000100000" when "0101",
-             "00000001000000" when "0110",
-             "00000010000000" when "0111",
-             "00000100000000" when "1000",
-             "00001000000000" when "1001",
-             "00010000000000" when "1010",
-             "00100000000000" when "1011",
-             "01000000000000" when "1100",
-             "10000000000000" when others;
+    DecSt <= "000000000000001" when "0000", -- fetch
+             "000000000000010" when "0001", -- decode
+             "000000000000100" when "0010", -- alu1
+             "000000000001000" when "0011", -- alu2
+             "000000000010000" when "0100", -- st
+             "000000000100000" when "0101", -- shift
+             "000000001000000" when "0110", -- jmp
+             "000000010000000" when "0111", -- call1
+             "000000100000000" when "1000", -- call2
+             "000001000000000" when "1001", -- call3
+             "000010000000000" when "1010", -- ret
+             "000100000000000" when "1011", -- push1
+             "001000000000000" when "1100", -- push2
+             "010000000000000" when "1101", -- pop
+             "100000000000000" when others; -- halt
 
   Type1 <= '1' when OP="0001" or OP="0011" or OP="0100" or -- LD/ADD/SUB
            OP="0101" or OP="0110" or OP="0111" or -- CMP/AND/OR
            OP="1000" else '0';                    -- XOR
-  
+
   NxtSt <= "0000" when (DecSt(0)='1' and Stop='1') or    -- Stop
-                       (DecSt(1)='1' and OP="0000") or   -- No
+                       (DecSt(1)='1' and OP="0000") or   -- NO
                        (DecSt(1)='1' and OP="1010" and JmpCnd='0') or -- JMP
-                       DecSt(3)='1' or DecSt(4)='1' or   -- LD/.../XOR/SHxx,ST
-                       DecSt(5)='1' or DecSt(7)='1' or   -- JMP,CALL
-                       DecSt(9)='1' or DecSt(11)='1' or  -- PUSH,POP
-                       DecSt(12)='1' or DecSt(13)='1' else -- RET,HALT
+                       DecSt(3)='1' or DecSt(4)='1' or DecSt(5)='1' or -- LD/.../XOR,SHxx,ST
+                       DecSt(6)='1' or DecSt(9)='1' or   -- JMP,CALL
+                       DecSt(12)='1' or DecSt(13)='1' or  -- PUSH,POP
+                       DecSt(10)='1' or DecSt(14)='1' else -- RET,HALT
            "0001" when DecSt(0)='1' and Stop='0'  else   -- Fetch
            "0010" when DecSt(1)='1' and Type1='1' else   -- LD/ADD/.../XOR
-           "0011" when (DecSt(1)='1' and OP="1001") or   -- SHIFT
-                       DecSt(2)='1' else                 -- LD/ADD/.../XOR
            "0100" when DecSt(1)='1' and OP="0010" else   -- ST
-           "0101" when DecSt(1)='1' and OP="1010" else   -- JMP/JZ/JC/JM
-           "0110" when DecSt(1)='1' and OP="1011" else   -- CALL
-           "0111" when DecSt(6)='1'               else
-           "1000" when DecSt(1)='1' and OP="1101" and Rx="00" else -- PUSH
-           "1001" when DecSt(8)='1'               else
-           "1010" when DecSt(1)='1' and OP="1101" and Rx="10" else -- POP
-           "1011" when DecSt(10)='1'                          else
-           "1100" when DecSt(1)='1' and OP="1110" else   -- RET
-           "1101";                                       -- HALT/ERROR
+           "0101" when DecSt(1)='1' and OP="1001" else   -- SHIFT
+           "0110" when DecSt(1)='1' and OP="1010" and JmpCnd='1' else -- JMP/JZ/JC/JM
+           "0111" when DecSt(1)='1' and OP="1011" else   -- CALL (1)
+           "1000" when DecSt(7)='1'               else   -- CALL (2)
+           "1001" when DecSt(8)='1'               else   -- CALL (3)
+           "1010" when DecSt(1)='1' and OP="1110" else   -- RET
+           "1011" when DecSt(1)='1' and OP="1101" and Rx="00" else -- PUSH (1)
+           "1100" when DecSt(11)='1'              else   -- PUSH (2)
+           "1101" when DecSt(1)='1' and OP="1101" and Rx="10" else -- POP
+           "1110";                                       -- HALT/ERROR
 
   process(Clk, Reset)
   begin
@@ -101,23 +101,23 @@ begin
       Stat <= NxtSt;
     end if;
   end process;
-  
+
   -- Control Signals
-      
+
   -- <WIP>
   LI   <= DecSt(0);
-  
+
   -- </WIP>
-      
+
   Jmp  <= '1' when Rd="00" else '0';  -- JMP
   Jz   <= '1' when Rd="01" else '0';  -- JZ
   Jc   <= '1' when Rd="10" else '0';  -- JC
   Jm   <= '1' when Rd="11" else '0';  -- JM
   Immd <= '1' when Rx="11" else '0';  -- Immediate mode
-  
+
   --        JMP     JZ and Z Flag       JC and C Flag       JM and S Flag
   JmpCnd <= Jmp or (Jz and Flag(0)) or (Jc and Flag(2)) or (Jm and Flag(1));
-  
+
   IrLd  <= DecSt(0);
   DrLd  <= DecSt(1) or (DecSt(2) and not Immd) or DecSt(10);
   FlgLd <= '1' when DecSt(3)='1' and OP/="0001" else '0';    -- OP /=LD
